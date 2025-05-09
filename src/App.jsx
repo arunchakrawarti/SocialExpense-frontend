8e3650c0066d1ac025dbf75c5a89b534b7cae975
arunchakrawarti <arunmojampur@gmail.com>
2025-05-10 01:13:20 +0530

    uploaded vercel json file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/register' element={<Signup/>}/>
+      <Route path='/navbar' element={<Navbar/>}/>
      </Routes>
      </BrowserRouter> 
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <h1>This is Navbar</h1>
+       <nav className='bg-blue-800 w-full h-20'>
+       <ul className="nav">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="#">Login</a>
+  </li>
+ 
+  <li className="nav-item">
+    <a className="nav-link disabled" aria-disabled="true">Register</a>
+  </li>
+</ul>
+
+       </nav>
     </div>
   )
 }
