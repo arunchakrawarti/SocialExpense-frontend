b9d92f10792b3140238d43fae21e3646484b6f60
arunchakrawarti <arunmojampur@gmail.com>
2025-05-11 18:49:54 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,6 +46,8 @@ const Navbar = (props) => {
         {data===true&&<li>
           <button onClick={handlelogout} className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</button>
         </li>}
+
+        
         
       </ul>
     </div>
